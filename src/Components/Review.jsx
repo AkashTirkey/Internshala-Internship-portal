1c1e8a257c23d1909001933f702baabca68c5444
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-05-16 17:10:30 +0530

    snall tweaks made

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -9,7 +9,7 @@ const Review = () => {
             <h2>Join the pool of 21Mn+ students and get started with your career</h2>
             <p>PLAY STORE RATING</p>
             <h3>4.4</h3>
-            <img src={Gplay} alt="Google Play" style={{display:"inline-block"}}/> <span> Get it on Google play </span>
+            <img src={Gplay} alt="Google Play"/>
         </div>
     </div>
   )
